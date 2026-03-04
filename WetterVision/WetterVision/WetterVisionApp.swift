7b543160b8428f7baecc60bf7cdaf464612cf2c4
MPC-Lab-3 <mpc-lab-3@iMac-von-MPC-Lab-3.local>
2026-03-04 14:01:59 +0100

    Add Xcode project structure

---
+++ WetterVision/WetterVision/WetterVisionApp.swift
@@ -0,0 +1,34 @@
+//
+//  WetterVisionApp.swift
+//  WetterVision
+//
+//  Created by MPC-Lab-3 on 04.03.26.
+//
+
+import SwiftUI
+
+@main
+struct WetterVisionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
