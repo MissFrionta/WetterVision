30a9df040dc19e73750b62ed599741ffee9b1aea
MPC-Lab-3 <mpc-lab-3@iMac-von-MPC-Lab-3.local>
2026-03-04 15:45:38 +0100

    new project created

---
+++ WetterApp/WetterApp/WetterAppApp.swift
@@ -0,0 +1,34 @@
+//
+//  WetterAppApp.swift
+//  WetterApp
+//
+//  Created by MPC-Lab-3 on 04.03.26.
+//
+
+import SwiftUI
+
+@main
+struct WetterAppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
